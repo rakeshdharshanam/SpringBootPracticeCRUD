51a605143cf67437c4406809b0b0a54c564ec4b2
rakesh <rakesh.dharshanam333@gmail.com>
2023-11-28 00:33:00 -0500

    cleaned

--- practice/src/main/java/com/example/practice/repository/studentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.practice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.practice.entity.student_entity;
-
-public interface studentRepository extends JpaRepository<student_entity, Long>{
-
-}
