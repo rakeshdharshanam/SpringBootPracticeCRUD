867fc0cc23d9373af0080e1829d979411c77ff8c
rakesh <rakesh.dharshanam333@gmail.com>
2023-11-28 00:37:48 -0500

    practice crud with pom

---
+++ src/main/java/com/example/practice/PracticeRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeRepoApplication.class, args);
+	}
+
+}
